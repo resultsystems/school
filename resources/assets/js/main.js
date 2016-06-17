8e6a32d11f432e3952aa28de460b365692ac61e7
Daniel Schmitz <danieljfa@gmail.com>
2016-06-17 08:01:59 -0300

    vue-routes

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-new Vue({
-  el: 'body',
-  components: { App }
+import VueRouter from 'vue-router'
+import Routes from './routes'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    linkActiveClass: 'active',
 })
+
+router.redirect({
+    '/': '/admin'
+})
+
+router.map(Routes)
+router.start(App, 'App')
+
---
+++ resources/assets/js/routes.js
@@ -0,0 +1,9 @@
+import Admin from './components/admin.vue'
+
+const Routes = {
+    '/admin': {
+        component: Admin
+    }
+}
+
+export default Routes;
