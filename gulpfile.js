c172a721eb1014b2983b1c615412fc7b62758219
daniel-schmitz <danieljfa@gmail.com>
2016-06-17 14:59:46 -0300

    fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,20 @@
 var elixir = require('laravel-elixir');
 
-elixir(function(mix) {
-    mix.scripts([
-              'app.js',
-                'config.js',
-                'controllers/*.js',
-                'modules/*.js',
-                'directives/*.js',
-                'services/*.js',
-                'menu.js'
-              ], 'public/js/app.js');
+elixir(function (mix) {
 
-    mix.scripts([
-            '../../../node_modules/angular-bootstrap-switch/dist/angular-bootstrap-switch.js',
-            '../../../node_modules/moment/moment.js',
-            '../../../node_modules/string-mask/src/string-mask.js',
-            '../../../node_modules/sweetalert/dist/sweetalert-dev.js',
-            '../../../node_modules/ng-file-upload/dist/ng-file-upload-shim.js',
-            '../../../node_modules/ng-file-upload/dist/ng-file-upload.js',
-              'dist/*.js',
-      ],'public/js/dist.js');
+  mix.browserify('main.js');
 
-    mix.copy([
-      'resources/assets/js/scripts/'
-      ],'public/js/scripts/');
+  mix.scripts([
+    '../../../node_modules/jquery/dist/jquery.js',
+    '../../../node_modules/moment/moment.js',
+    '../../../node_modules/materialize-css/dist/js/materialize.js'
+  ], 'public/js/dist.js');
+
+  mix.styles([
+    "../../../node_modules/materialize-css/dist/css/materialize.css",
+    "main.css"
+  ],'public/css/dist.css');
+
+  mix.copy('node_modules/materialize-css/dist/fonts', 'public/fonts');
+
 });
