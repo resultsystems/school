45745f67efbc96e7e1dc6b29ea7807051138af4b
Daniel Schmitz <danieljfa@gmail.com>
2016-06-18 09:01:25 -0300

    login com o tipo de usuário

--- resources/assets/js/components/login.vue
+++ resources/assets/js/components/login.vue
@@ -1,60 +1,73 @@
 <template>
 
-<div class="container">
+  <div class="container">
     <div class="row">
-        <div class="col s12 m8 offset-m2">
-            <form class="login-form">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="img/login.png">
-                        <span class="card-title">
+      <div class="col s12 m8 offset-m2">
+        <form class="login-form">
+          <div class="card">
+            <div class="card-image">
+              <img src="img/login.png">
+              <span class="card-title">
                         <h2>Login</h2>
                         <h6>Resultsystems / School</h6>
                     </span>
-                    </div>
-                    <div class="card-content">
-                        <div class="input-field">
-                            <input id="username" type="text" v-model="username" :value="getLogin.user.username">
-                            <label for="username">Usuário</label>
-                        </div>
+            </div>
+            <div class="card-content">
 
-                        <div class="row">
-                            <div class="col s12 m8 l9">
-                                <div class="input-field">
-                                    <input id="password" type="password" v-model="password">
-                                    <label for="password">Senha</label>
-                                </div>
-                            </div>
-                            <div class="col s12 m4 l3">
-                                <div class="input-field">
-                                    <input type="checkbox" id="remember-me" @click="message('tyesye')" />
-                                    <label for="remember-me">Lembrar</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+              <div class="row">
 
-                    <div class="card-action blue-grey lighten-3">
-                        <div class="center-align">
-                            <a @click="tryLogin" class="btn blue-grey darken-1" v-bind:class="{ 'disabled': loading }"><i class="material-icons left">vpn_key</i>Login</a>
-                        </div>
-                          <div class="progress" v-show="loading">
-                            <div class="indeterminate"></div>
-                        </div>
-                    </div>
+                <div class="col s12 m8">
+                  <div class="input-field">
+                    <input id="username" type="text" v-model="username" :value="getLogin.user.username">
+                    <label for="username">Usuário</label>
+                  </div>
                 </div>
-            </form>
-            <div class="row">
-                <div class="col s4">
-                    <a href="#">Registrar</a>
+                <div class="col s12 m4">
+                  <label>Tipo</label>
+                  <select v-model="ownertype">
+                          <option value="Employee">Funcionário</option>
+                          <option value="Teacher">Professor</option>
+                          <option value="Student">Aluno</option>
+                   </select>
                 </div>
-                <div class="col s8 right-align">
-                    <a href="#" class="">Esqueci a senha</a>
+              </div>
+              <div class="row">
+                <div class="col s12 m8 l9">
+                  <div class="input-field">
+                    <input id="password" type="password" v-model="password">
+                    <label for="password">Senha</label>
+                  </div>
                 </div>
+                <div class="col s12 m4 l3">
+                  <div class="input-field">
+                    <input type="checkbox" id="remember-me" @click="message('tyesye')" />
+                    <label for="remember-me">Lembrar</label>
+                  </div>
+                </div>
+              </div>
             </div>
 
+            <div class="card-action blue-grey lighten-3">
+              <div class="center-align">
+                <a @click="tryLogin" class="btn blue-grey darken-1" v-bind:class="{ 'disabled': loading }"><i class="material-icons left">vpn_key</i>Login</a>
+              </div>
+              <div class="progress" v-show="loading">
+                <div class="indeterminate"></div>
+              </div>
+            </div>
+          </div>
+        </form>
+        <div class="row">
+          <div class="col s4">
+            <a href="#">Registrar</a>
+          </div>
+          <div class="col s8 right-align">
+            <a href="#" class="">Esqueci a senha</a>
+          </div>
+        </div>
+
+      </div>
     </div>
-</div>
 
 
 </template>
@@ -68,6 +81,7 @@ export default{
         return {
             username:'',
             password:'',
+            ownertype:'Teacher',
             loading:false
         }
     },
@@ -79,10 +93,34 @@ export default{
             getLogin
         }
     },
+    ready()
+    {
+       /* SELECT HOOK */
+       /* https://github.com/Dogfalo/materialize/issues/2838 */
+       $("select").val(this.ownertype);
+       var suspend = false;
+       $("select").material_select();
+       $('select').on('change', function() {
+         console.log(suspend)
+         if (!suspend) {
+            suspend = true;
+          var event = new CustomEvent('change', {
+            detail: 'change',
+            bubbles: true
+          });
+          $(this).get(0).dispatchEvent(event);
+        }else{suspend=false}
+       });
+       /* END */
+    },
     methods:    {
         tryLogin(){
             this.loading = true
-            let user = {'username':this.username,'password':this.password}
+            let user = {'username':this.username,
+                                      'password':this.password,
+                                      'type':this.ownertype
+                                  }
+                                  console.log(user);
             this.doLogin(user,(r)=>{
                 this.$data.loading=false;
             })
